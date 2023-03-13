c70eb913e9099b1080cf4e4fff7e1020bdc9130b
Linde <1030907322@qq.com>
2023-03-13 17:19:00 +0800

    SpringMVC入门

---
+++ SpringMVC_Part1/src/main/java/com/linde/config/SpringMvcConfig.java
@@ -0,0 +1,10 @@
+package com.linde.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.linde.controller")
+public class SpringMvcConfig {
+
+}
